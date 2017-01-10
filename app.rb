8910da20315469f4cd64178b5fc985835fed6ee5
AfkZa4aem <nastypinagrably@gmail.com>
2017-01-10 14:45:58 +0300

    add comments

--- app.rb
+++ app.rb
@@ -9,12 +9,16 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# call everytime when any page was refreshed
 before do
+	# db initialize
 	init_db
 end
-
+# call everytime - configuration app (code change or page refresh)
 configure do
+	# db initialize
 	init_db
+	# create table if table is not exist
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
 		(
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +36,7 @@ get '/new' do
 end
 
 post '/new' do
+	# get var from POST-request
   content = params[:content]
 
   erb "You typed #{content}"
