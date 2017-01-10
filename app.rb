da993a3dc02b6c52789aa95c4fd4ebc9d6a0e429
AfkZa4aem <nastypinagrably@gmail.com>
2017-01-10 14:50:16 +0300

    add parameter validation

--- app.rb
+++ app.rb
@@ -39,5 +39,10 @@ post '/new' do
 	# get var from POST-request
   content = params[:content]
 
+  if content.length <= 0
+  	@error = "Type post text"
+  	return erb :new
+  end
+
   erb "You typed #{content}"
 end
