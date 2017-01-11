38bf03040f9c724a015713afe4f07aa049394568
AfkZa4aem <nastypinagrably@gmail.com>
2017-01-11 14:15:43 +0300

    save comment to db

--- app.rb
+++ app.rb
@@ -77,5 +77,7 @@ post '/details/:post_id' do
 	post_id = params[:post_id]
 	content = params[:content]
 
-	erb "You typed comment #{content} fo post #{post_id}"
+	@db.execute 'insert into Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
+
+	redirect to('/details/' + post_id)
 end
